a2a56310353a7b26391c61bad4543471dabfbed7
josemibnf <josemi.bnf@outlook.es>
2020-07-08 11:41:44 +0200

    concurrencia

--- concurrencia.py
+++ concurrencia.py
@@ -1,32 +1,38 @@
 from threading import Thread
+import time
 
-def message1():
-    wait(4)
+def message1(threads):
+    time.sleep(1)
+    threads[3].join() # Se bloquea hasta que termine el hilo 4.
     print('“are made of coffee?')
 
 def message2():
     print('Do you know')
 
-def message3():
-    wait(5)
+def message3(threads):
+    time.sleep(1)
+    threads[4].join() # Se bloquea hasta que termine el hilo 5.
     print('it!')
 
-def message4():
-    wait(2)
+def message4(threads):
+    threads[1].join() # Se bloquea hasta que termine el hilo 2.
     print(' that our bodies')
 
-def message5():
-    wait(1)
+def message5(threads):
+    threads[0].join() # Se bloquea hasta que termine el hilo 1.
     print('-Try drinking')
 
 def concurrency_with_coffe():
     print("- I'm tired, Bob!")
-    threads = [ Thread(target=message1()), Thread(target=message2()), 
-                Thread(target=message3()), Thread(target=message4()), 
-                Thread(target=message5())
-             ]
+    threads = []
+    threads.extend( [ Thread(target=message1, args=(threads,)), Thread(target=message2), 
+                Thread(target=message3, args=(threads,)), Thread(target=message4, args=(threads,)), 
+                Thread(target=message5, args=(threads,))
+             ] )
     for thread in threads:
         thread.start()
-    for thread in threads:
-        thread.join()
+    threads[2].join()
     print('- You’re right!')
+
+if __name__ == "__main__":
+    concurrency_with_coffe()
