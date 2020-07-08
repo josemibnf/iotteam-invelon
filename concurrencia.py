83e4cdd5d15548dc73751b1e8ac0c49a406bd2ab
josemibnf <josemi.bnf@outlook.es>
2020-07-08 12:19:39 +0200

    using an event

--- concurrencia.py
+++ concurrencia.py
@@ -1,16 +1,16 @@
-from threading import Thread
+from threading import Thread, Event
 import time
 
-def message1(threads):
-    time.sleep(1)
+def message1(threads, event):
+    event.wait()
     threads[3].join() # Se bloquea hasta que termine el hilo 4.
     print('“are made of coffee?')
 
 def message2():
     print('Do you know')
 
-def message3(threads):
-    time.sleep(1)
+def message3(threads, event):
+    event.wait()
     threads[4].join() # Se bloquea hasta que termine el hilo 5.
     print('it!')
 
@@ -24,13 +24,15 @@ def message5(threads):
 
 def concurrency_with_coffe():
     print("- I'm tired, Bob!")
+    event = Event()
     threads = []
-    threads.extend( [ Thread(target=message1, args=(threads,)), Thread(target=message2), 
-                Thread(target=message3, args=(threads,)), Thread(target=message4, args=(threads,)), 
+    threads.extend( [ Thread(target=message1, args=(threads,event)), Thread(target=message2), 
+                Thread(target=message3, args=(threads,event)), Thread(target=message4, args=(threads,)), 
                 Thread(target=message5, args=(threads,))
              ] )
     for thread in threads:
         thread.start()
+    event.set()
     threads[2].join()
     print('- You’re right!')
 
