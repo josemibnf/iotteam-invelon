591f3dc6f0eaa81e7405806a78bd6650c7ebff5f
josemibnf <josemi.bnf@outlook.es>
2020-07-08 12:29:11 +0200

    ..

--- concurrencia.py
+++ concurrencia.py
@@ -2,29 +2,29 @@ from threading import Thread, Event
 import time
 
 def message1(threads, event):
-    event.wait()
+    event.wait()    # Espera a que se haya iniciado el hilo 5 (para esperar directamente al 4 se podria haber usado otro evento).
     threads[3].join() # Se bloquea hasta que termine el hilo 4.
-    print('“are made of coffee?')
+    print(' are made of coffee?')
 
 def message2():
-    print('Do you know')
+    print('  Do you know',end="")
 
 def message3(threads, event):
-    event.wait()
+    event.wait()    # Espera a que se haya iniciado el hilo 5.
     threads[4].join() # Se bloquea hasta que termine el hilo 5.
-    print('it!')
+    print(' it!')
 
 def message4(threads):
     threads[1].join() # Se bloquea hasta que termine el hilo 2.
-    print(' that our bodies')
+    print(' that our bodies',end="")
 
 def message5(threads):
     threads[0].join() # Se bloquea hasta que termine el hilo 1.
-    print('-Try drinking')
+    print('-Try drinking',end="")
 
 def concurrency_with_coffe():
     print("- I'm tired, Bob!")
-    event = Event()
+    event = Event() # Se usa para que los hilos 1 y 3 no puedan llamar a hilos aun no creados (4y5).
     threads = []
     threads.extend( [ Thread(target=message1, args=(threads,event)), Thread(target=message2), 
                 Thread(target=message3, args=(threads,event)), Thread(target=message4, args=(threads,)), 
@@ -32,7 +32,7 @@ def concurrency_with_coffe():
              ] )
     for thread in threads:
         thread.start()
-    event.set()
+    event.set() # Indica que ya estan iniciados todos los hilos.
     threads[2].join()
     print('- You’re right!')
 
