32890270923c2d47ae5d91a685230ae6c2921caa
josemibnf <josemi.bnf@outlook.es>
2020-07-09 18:45:42 +0200

    models registered in admin

--- CoffeControl/apps/logistics/admin.py
+++ CoffeControl/apps/logistics/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CoffeType, CoffeeStock, CoffeeOrder
 
 # Register your models here.
+admin.site.register(CoffeType)
+admin.site.register(CoffeeStock)
+admin.site.register(CoffeeOrder)
