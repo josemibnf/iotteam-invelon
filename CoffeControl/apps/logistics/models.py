6cf6f089ec3e424a01506b3ed43f5fef7804ef74
josemibnf <josemi.bnf@outlook.es>
2020-07-09 18:11:28 +0200

    CoffeType advanced

--- CoffeControl/apps/logistics/models.py
+++ CoffeControl/apps/logistics/models.py
@@ -7,10 +7,25 @@ class CoffeType(models.Model):
     name = models.CharField(max_length=30)
     current_price = models.FloatField(validators=[MinValueValidator(0)])
 
+    def save(self, *args, **kwargs):
+        super(CoffeType, self).save(*args, **kwargs)
+        for department in 'business.Department'.objects.all():
+            try:
+                coffe_stock = CoffeeStock(department=department, coffe_type=self)
+                coffe_stock.save()
+            except AssertionError:
+                print('Ya existe un CoffeeStock para ese par Deparamento/CoffeType')
+
 class CoffeeStock(models.Model):
     current_units = models.IntegerField(validators=[MinValueValidator(0)])
     coffe_type = models.ForeignKey(CoffeType, on_delete=models.CASCADE)
     department = models.ForeignKey('business.Department', on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if self.pk == None: # Solo si es un objeto nuevo.
+            assert(  )
+        super(CoffeeStock, self).save(*args, **kwargs)
+        
 
 class CoffeeOrder(models.Model):
     unit_price = models.FloatField(validators=[MinValueValidator(0)])
