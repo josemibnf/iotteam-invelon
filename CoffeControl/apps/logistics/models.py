4ec222a2d3923cec0d8dbb7b508ba508d14aee8b
josemibnf <josemi.bnf@outlook.es>
2020-07-09 15:08:21 +0200

    logistics models added

--- CoffeControl/apps/logistics/models.py
+++ CoffeControl/apps/logistics/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from django.apps import apps
 
 # Create your models here.
+class CoffeType(models.Model):
+    name = models.CharField(max_length=30)
+    current_price = models.FloatField(validators=[MinValueValidator(0)])
+
+class CoffeeStock(models.Model):
+    current_units = models.IntegerField(validators=[MinValueValidator(0)])
+    coffe_type = models.ForeignKey(CoffeType, on_delete=models.CASCADE)
+    department = models.ForeignKey('business.Department', on_delete=models.CASCADE)
+
+class CoffeeOrder(models.Model):
+    unit_price = models.FloatField(validators=[MinValueValidator(0)])
+    units = models.IntegerField(validators=[MinValueValidator(0)])
+    status = models.CharField(max_length=8, choices=[('Pending', 'Pending'), ('Accepted', 'Accepted')] , default='Pending')
+    coffee_stock = models.ForeignKey(CoffeeStock, on_delete=models.CASCADE)
